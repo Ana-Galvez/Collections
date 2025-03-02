dac37fc87b918cd172e6be4f9b581c013bdf8a36
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-03-01 21:05:38 -0300

    feat: agregar datos y mostrarlos

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -7,6 +7,23 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            List<int> numeros = new List<int>();
+            numeros.Add(1);
+            numeros.Add(1);
+            numeros.Add(45);
+            numeros.Add(8);
+            numeros.Add(18);
+            numeros.Add(101);
+            numeros.Add(202);
+            numeros.Add(123);
+
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+
+
         }
     }
 }
